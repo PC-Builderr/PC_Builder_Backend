03d45c2d0e927daa9bc9d81e7ee867d62227e5f3
Valentin <valentinspasov9669@gmail.com>
2020-11-20 16:28:57 +0200

    [FIX] useFetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { ProductCard } from './components/ProductCard'
 import { useFetch } from './hooks/useFetch'
 
 export const App = props => {
     const [value, setValue] = useState('')
-    const [data, loading, error] = useFetch(value)
+    const [data, loading, error, fetchData] = useFetch()
+
+    useEffect(() => {
+        fetchData(`product?filters=${value}`, { method: 'GET' })
+    }, [value, fetchData])
+
+    useEffect(() => {
+        console.log(`Data: ${data} --- Loading: ${loading} --- Error: ${error}`)
+    }, [data, loading, error])
+
     return (
-        <>
-            <input type='text' value={value} onChange={e => setValue(e.target.value)} />
-            <Switch>
-                <Route path='/1' exact>
-                    <div>
-                        {loading ? (
-                            <h1>LOADING...</h1>
-                        ) : (
-                            error.message ||
-                            data?.products.map(p => {
-                                return <ProductCard key={p.id} product={p} />
-                            })
-                        )}
-                    </div>
-                </Route>
-            </Switch>
-        </>
+        <Switch>
+            <Route path='/1' exact>
+                <div>
+                    <input type='text' value={value} onChange={e => setValue(e.target.value)} />
+                    {loading ? (
+                        <h1>LOADING...</h1>
+                    ) : (
+                        error ||
+                        data?.products.map(p => {
+                            return <ProductCard key={p.id} product={p} />
+                        })
+                    )}
+                </div>
+            </Route>
+        </Switch>
     )
 }
--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,33 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 
-export const useFetch = (resource, options = { method: 'GET', headers: null, body: null }) => {
-    const [loading, setloading] = useState(true)
-    const [error, setError] = useState(null)
-    const [data, setData] = useState(null)
-    useEffect(() => {
-        let response
-        setloading(true)
-        fetch(`http://localhost:4000/api/${resource}`, {
-            method: options.method,
-            headers: {
-                'Content-Type': 'application/json',
-                ...options.headers
-            },
-            body: options.body
-        })
-            .then(res => {
-                response = res
-                return res.json()
-            })
-            .then(resData => {
-                if (!response.ok) throw new Error(resData.message)
-                setData(resData)
-            })
-            .catch(error => setError(error))
-            .finally(() => {
-                setloading(false)
-            })
-    }, [resource, options.headers, options.method, options.body])
+export const useFetch = () => {
+    const [state, setState] = useState({ data: null, loading: true, error: null })
 
-    return [data, loading, error]
+    const fetchData = useCallback(
+        (resource, options = { method: 'GET', headers: null, body: null }) => {
+            let response
+            setState(currentState => {
+                return {
+                    data: currentState.data,
+                    loading: true,
+                    error: null
+                }
+            })
+            fetch(`http://localhost:4000/api/${resource}`, {
+                method: options.method,
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...options.headers
+                },
+                body: options.body
+            })
+                .then(res => {
+                    response = res
+                    return res.json()
+                })
+                .then(resData => {
+                    if (!response.ok) throw new Error(resData.message)
+                    setState({ data: resData, loading: false, error: null })
+                })
+                .catch(error => setState({ data: null, loading: false, error: error.message }))
+        },
+        []
+    )
+
+    return [state.data, state.loading, state.error, fetchData]
 }
